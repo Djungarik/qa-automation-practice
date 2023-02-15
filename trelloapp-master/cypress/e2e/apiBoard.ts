1be1b792ff2f69f2ff6852a958f01ef0d4d12335
Djungarik <berezhnoy.tim@gmail.com>
2023-02-15 12:18:21 +0200

    add api delete board test

--- trelloapp-master/cypress/e2e/apiBoard.ts
+++ trelloapp-master/cypress/e2e/apiBoard.ts
@@ -12,4 +12,15 @@ describe("API testing", () => {
       expect(status).to.eq(201);
     });
   });
+
+  it("deletes a board", () => {
+    cy.apiAddBoard("some cool project").then((id) => {
+      cy.request({
+        method: "DELETE",
+        url: `/api/boards/${id}`,
+      }).then(({ status }) => {
+        expect(status).to.eq(200);
+      });
+    });
+  });
 });
